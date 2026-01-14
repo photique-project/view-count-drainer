d65cb5e15f4db245b82e213192bfd67507e58fc8
BenchPress200 <jms0538@naver.com>
2026-01-14 20:48:54 +0900

    init

---
+++ src/main/java/com/benchpress200/viewcountdrainer/ViewCountDrainerApplication.java
@@ -0,0 +1,13 @@
+package com.benchpress200.viewcountdrainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewCountDrainerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViewCountDrainerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/benchpress200/viewcountdrainer/ViewCountDrainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benchpress200.viewcountdrainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViewCountDrainerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
