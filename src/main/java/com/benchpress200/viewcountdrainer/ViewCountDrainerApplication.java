20476e020d80f43d74c0e9606ba322db1e3f7668
BenchPress200 <jms0538@naver.com>
2026-01-14 20:53:43 +0900

    config: 스케줄링 허용 & 레디스 설정 클래스 추가

--- src/main/java/com/benchpress200/viewcountdrainer/ViewCountDrainerApplication.java
+++ src/main/java/com/benchpress200/viewcountdrainer/ViewCountDrainerApplication.java
@@ -2,7 +2,9 @@ package com.benchpress200.viewcountdrainer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class ViewCountDrainerApplication {
 
